dd327d67b11904f3a896e9df73018217311d8104
Tom Arrow <bomarrow1@gmail.com>
2016-04-01 15:22:34 +0100

    Addshore broke this

--- addpapers.py
+++ addpapers.py
@@ -7,13 +7,45 @@ import queue
 import threading
 import time
 
-def worker():
+q=queue.Queue()
+
+def doStuff():
+    citefile = queryCiteFile.CiteFile()
+    citations = citefile.findRowsWithIDType('pmc')
+    for idx, citation in enumerate(citations[10513:]):
+        doOneThing(idx, citation)
+
+def doStuffThreaded():
+    threads = []
+    for i in range(10):
+        t = threading.Thread(target=actuallyDoStuffThreaded())
+        t.start()
+        threads.append(t)
+
+    citefile = queryCiteFile.CiteFile()
+    citations = citefile.findRowsWithIDType('pmc')
+    for citation in enumerate(citations[10513:]):
+        q.put(citation)
+
+    q.join()
+
+    for i in range(10):
+        q.put(None)
+    for t in threads:
+        t.join()
+
+
+def actuallyDoStuffThreaded():
     while True:
-        start=time.time()
         idx, citation = q.get()
+        doOneThing( idx, citation )
+        q.task_done()
+
+def doOneThing( idx, citation ):
+        start=time.time()
         print(citation)
         if citation is None:
-            break
+            return
         print('trying to add {} number {}'.format(citation[5], idx))
         site = pywikibot.Site("librarybase", "librarybase")
         item = librarybase.JournalArticlePage(site)
@@ -22,7 +54,7 @@ def worker():
             pmcidobj.getBBasicMetadata()
         except IDNotResolvedException:
             print('Couldn\'t find in EPMC:' + citation[5])
-            continue
+            return
         metadata = pmcidobj.metadata
         print("Got metadata in:" + str(time.time()-start))
         if not item.articleAlreadyExists(metadata['pmcid']):
@@ -31,23 +63,3 @@ def worker():
             print("set metadata in" + str(time.time()-start))
         else:
             print("{} already exists. Doing nothing".format(metadata['pmcid']))
-        q.task_done()
-
-# q=queue.Queue()
-# threads = []
-# for i in range(10):
-#     t = threading.Thread(target=worker)
-#     t.start()
-#     threads.append(t)
-
-citefile = queryCiteFile.CiteFile()
-citations = citefile.findRowsWithIDType('pmc')
-for citation in enumerate(citations[10513:]):
-    worker(citation)
-
-# q.join()
-#
-# for i in range(10):
-#     q.put(None)
-# for t in threads:
-#     t.join()
